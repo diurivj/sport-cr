9f7b52e8f672419e66b001bcb3f3cb8198abaea2
diurivj <diurivj@icloud.com>
2019-06-28 12:50:56 -0500

    done x2

--- controllers/sports.controller.js
+++ controllers/sports.controller.js
@@ -6,3 +6,20 @@ exports.findSports = (req, res, next) => {
     .then(sports => res.render('sports/all', { sports }))
     .catch(err => res.render('sports/all', err))
 }
+
+exports.viewCreateSport = (req, res, next) => {
+  res.render('sports/create')
+}
+
+exports.createSport = (req, res, next) => {
+  Sport.create({ ...req.body, players: Number(req.body.players) })
+    .then(sport => res.redirect('/sports'))
+    .catch(err => res.send(err))
+}
+
+exports.deleteSport = (req, res, next) => {
+  const { id } = req.params
+  Sport.findByIdAndRemove(id)
+    .then(() => res.redirect('/sports'))
+    .catch(err => res.send(err))
+}
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { findSports } = require('../controllers/sports.controller')
+const { findSports, viewCreateSport, createSport, deleteSport } = require('../controllers/sports.controller')
 const { findTeams } = require('../controllers/teams.controller')
 const { findPlayers } = require('../controllers/players.controller')
 
@@ -9,6 +9,13 @@ router.get('/', (req, res, next) => res.render('index'))
 
 /* GET sports page */
 router.get('/sports', findSports)
+
+/* GET create sports page */
+router.get('/sports/new', viewCreateSport)
+/* POST create sports page */
+router.post('/sports/new', createSport)
+
+router.get('/sports/:id/delete', deleteSport)
 
 /* GET teams page */
 router.get('/teams', findTeams)
